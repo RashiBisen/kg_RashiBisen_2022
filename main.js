10e77ccbf5294b88e1238327dbb06c1547915fbe
Rashi <rashi3196@gmail.com>
2021-05-03 17:34:38 -0400

    created map for digits and iterated over each number's digit to create output string as described in output format in pdf

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-var num_array = process.argv.slice(2)
-console.log(num_array)
+let digits = new Map()
+digits.set(0, "Zero")
+digits.set(1, "One")
+digits.set(2, "Two")
+digits.set(3, "Three")
+digits.set(4, "Four")
+digits.set(5, "Five")
+digits.set(6, "Six")
+digits.set(7, "Seven")
+digits.set(8, "Eight")
+digits.set(9, "Nine")
+var num_array = process.argv.slice(2);
+var word_string = "";
 var i;
 for(i=0;i<num_array.length;i++)
 {
 	var num = num_array[i];
 	var arr = Array.from(String(num), Number);
-	console.log(arr);
+	var j;
+	var temp_num ="";
+	for(j=0;j<arr.length;j++)
+	{
+		temp_num += digits.get(arr[j]);
+	}
+	word_string += temp_num;
+	word_string += ",";
 }
+word_string = word_string.slice(0, -1)
+console.log(word_string);
